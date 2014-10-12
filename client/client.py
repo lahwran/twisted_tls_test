2d05132cfc4d537e0b7e00ae571779ebc937da4b
Chris Horne <lahwran@lahwran.net>
2014-10-11 23:50:35 -0600

    test of thing - works, need to test if bad certs are rejected

--- client/client.py
+++ client/client.py
@@ -4,21 +4,41 @@ import os; os.chdir(os.path.dirname(__file__))
 import sys
 
 from twisted.internet.protocol import Protocol, Factory
-from twisted.internet.endpoints import TCP4ClientEndpoint
+from twisted.internet.endpoints import SSL4ClientEndpoint
+from twisted.internet import ssl
 from twisted.internet import reactor
+from twisted.python import log
 
 class Echo(Protocol):
     def connectionMade(self):
         self.transport.write("Hello server, I am the client!\n")
         reactor.callLater(3, self.transport.loseConnection)
 
+    def connectionLost(self, derp):
+        print "connection lost"
+
     def dataReceived(self, data):
         sys.stdout.write(data)
 
+def get_endpoint():
+    client = sys.argv[1]
+    ca = sys.argv[2]
+    print "using client identification", client
+    clientdata = (
+            open("./" + client + ".crt.pem", "r").read()
+          + open("./" + client + ".key.pem", "r").read()
+    )
+    cadata = open("../" + ca + ".crt.pem", "r").read()
+    cert = ssl.PrivateCertificate.loadPEM(clientdata)
+    authority = ssl.Certificate.loadPEM(cadata)
+    return SSL4ClientEndpoint(reactor, "localhost", 8006,
+            cert.options(authority))
+
 def main():
+    log.startLogging(sys.stdout)
     print "main"
-    point = TCP4ClientEndpoint(reactor, "localhost", 8007)
-    d = point.connect(Factory.forProtocol(Echo))
+    endpoint = get_endpoint()
+    endpoint.connect(Factory.forProtocol(Echo))
     print "running reactor"
     reactor.run()
 
--- server/server.py
+++ server/server.py
@@ -4,20 +4,45 @@ import os; os.chdir(os.path.dirname(__file__))
 import sys
 
 from twisted.internet.protocol import Protocol, Factory
-from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.endpoints import SSL4ServerEndpoint
+from twisted.internet import ssl
 from twisted.internet import reactor
+from twisted.python import log
 
 class Echo(Protocol):
     def connectionMade(self):
         self.transport.write("Hello client, I am the server!\n")
         reactor.callLater(3, self.transport.loseConnection)
 
+    def connectionLost(self, derp):
+        print "connection lost"
+
     def dataReceived(self, data):
         sys.stdout.write(data)
 
+
+def get_endpoint():
+    serv = sys.argv[1]
+    ca = sys.argv[2]
+    print "using server identification", serv
+    servdata = (
+            open("./" + serv + ".crt.pem", "r").read()
+          + open("./" + serv + ".key.pem", "r").read()
+    )
+    cadata = open("../" + ca + ".crt.pem", "r").read()
+    cert = ssl.PrivateCertificate.loadPEM(servdata)
+    authority = ssl.Certificate.loadPEM(cadata)
+    endpoint = SSL4ServerEndpoint(reactor,
+            8006, cert.options(authority))
+    return endpoint
+
+
 def main():
+    log.startLogging(sys.stdout)
     print "main"
-    endpoint = TCP4ServerEndpoint(reactor, 8007)
+
+    endpoint = get_endpoint()
+
     endpoint.listen(Factory.forProtocol(Echo))
     print "running reactor"
     reactor.run()
